6bbcd2a6f1aa06a2ec3005463ea700bb73cc68e9
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:08 -0500

    TUnit conversion

--- test/Soenneker.Adyen.OpenApiClient.Tests/AdyenOpenApiClientTests.cs
+++ test/Soenneker.Adyen.OpenApiClient.Tests/AdyenOpenApiClientTests.cs
@@ -1,16 +1,15 @@
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Adyen.OpenApiClient.Tests;
 
-[Collection("Collection")]
-public sealed class AdyenOpenApiClientTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class AdyenOpenApiClientTests : HostedUnitTest
 {
-    public AdyenOpenApiClientTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public AdyenOpenApiClientTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
