d04e7a15afa76e5fad3e647ef0bf0c29e728c7b7
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-08-01 18:04:37 +0000

    Automated update

--- src/Soenneker.Adyen.OpenApiClient/LegalEntityServiceV4/LegalEntities/Item/RequestPeriodicReview/RequestPeriodicReviewRequestBuilder.cs
+++ src/Soenneker.Adyen.OpenApiClient/LegalEntityServiceV4/LegalEntities/Item/RequestPeriodicReview/RequestPeriodicReviewRequestBuilder.cs
@@ -36,20 +36,20 @@ namespace Soenneker.Adyen.OpenApiClient.LegalEntityServiceV4.LegalEntities.Item.
         /// <summary>
         /// Requests a periodic data review for the legal entity of the user specified in the path.
         /// </summary>
-        /// <returns>A <see cref="global::Soenneker.Adyen.OpenApiClient.Models.DefaultResponseResponseJson"/></returns>
+        /// <returns>A <see cref="global::Soenneker.Adyen.OpenApiClient.Models.LegalEntityServiceV4PostLegalEntitiesIdRequestPeriodicReview201Response"/></returns>
         /// <param name="cancellationToken">Cancellation token to use when cancelling requests</param>
         /// <param name="requestConfiguration">Configuration for the request such as headers, query parameters, and middleware options.</param>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
-        public async Task<global::Soenneker.Adyen.OpenApiClient.Models.DefaultResponseResponseJson?> PostAsync(Action<RequestConfiguration<DefaultQueryParameters>>? requestConfiguration = default, CancellationToken cancellationToken = default)
+        public async Task<global::Soenneker.Adyen.OpenApiClient.Models.LegalEntityServiceV4PostLegalEntitiesIdRequestPeriodicReview201Response?> PostAsync(Action<RequestConfiguration<DefaultQueryParameters>>? requestConfiguration = default, CancellationToken cancellationToken = default)
         {
 #nullable restore
 #else
-        public async Task<global::Soenneker.Adyen.OpenApiClient.Models.DefaultResponseResponseJson> PostAsync(Action<RequestConfiguration<DefaultQueryParameters>> requestConfiguration = default, CancellationToken cancellationToken = default)
+        public async Task<global::Soenneker.Adyen.OpenApiClient.Models.LegalEntityServiceV4PostLegalEntitiesIdRequestPeriodicReview201Response> PostAsync(Action<RequestConfiguration<DefaultQueryParameters>> requestConfiguration = default, CancellationToken cancellationToken = default)
         {
 #endif
             var requestInfo = ToPostRequestInformation(requestConfiguration);
-            return await RequestAdapter.SendAsync<global::Soenneker.Adyen.OpenApiClient.Models.DefaultResponseResponseJson>(requestInfo, global::Soenneker.Adyen.OpenApiClient.Models.DefaultResponseResponseJson.CreateFromDiscriminatorValue, default, cancellationToken).ConfigureAwait(false);
+            return await RequestAdapter.SendAsync<global::Soenneker.Adyen.OpenApiClient.Models.LegalEntityServiceV4PostLegalEntitiesIdRequestPeriodicReview201Response>(requestInfo, global::Soenneker.Adyen.OpenApiClient.Models.LegalEntityServiceV4PostLegalEntitiesIdRequestPeriodicReview201Response.CreateFromDiscriminatorValue, default, cancellationToken).ConfigureAwait(false);
         }
         /// <summary>
         /// Requests a periodic data review for the legal entity of the user specified in the path.
